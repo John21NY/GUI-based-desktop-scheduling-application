7f83c323d9b744a38c05f1b4d981cda2bce324d0
John_21NY <intoul3@wgu.edu>
2022-04-11 11:11:33 -0400

    second save

---
+++ src/controller/LoginForm.java
@@ -0,0 +1,27 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginForm {
+    public Label titleLabel;
+    public Label usernameLabel;
+    public TextField usernameTextBox;
+    public Label passwordLabel;
+    public TextField passwordTextBox;
+    public Button loginButton;
+    public Button clearButton;
+    public Button exitButton;
+    public Label zoneLabel;
+
+    public void LogonButton(ActionEvent actionEvent) {
+    }
+
+    public void ClearButton(ActionEvent actionEvent) {
+    }
+
+    public void ExitButton(ActionEvent actionEvent) {
+    }
+}
